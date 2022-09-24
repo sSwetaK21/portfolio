ed4ed5d571a9530b637792a5ec6fae13433565b9
shweta <shwetakodamg@gmail.com>
2022-09-24 23:11:49 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let togglebtn = document.querySelector(".side_toggle")
+let closebtn =document.querySelector(".closebtn")
+let asidebar = document.querySelector(".aside_bar")
+
+togglebtn.addEventListener("click", function (){
+    asidebar.classList.toggle("show-sidebar")
+})
+
+closebtn.addEventListener("click", function (){
+    asidebar.classList.remove("show-sidebar")
+
+})
